88154f0c8011794e00cc198e0eb81c9a1985649a
Marcell <80519629+KisKova@users.noreply.github.com>
2023-11-10 13:05:04 +0100

    Initial commit

---
+++ src/test/java/via/doc1/dockertest/DockerTestApplicationTests.java
@@ -0,0 +1,13 @@
+package via.doc1.dockertest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DockerTestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
