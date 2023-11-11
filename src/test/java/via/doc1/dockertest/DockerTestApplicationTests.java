10e28f1c4235cbf75ccb2fe6198335cc755e170b
Marcell <80519629+KisKova@users.noreply.github.com>
2023-11-11 21:57:35 +0100

    CI/CD Workflow

--- src/test/java/via/doc1/dockertest/DockerTestApplicationTests.java
+++ src/test/java/via/doc1/dockertest/DockerTestApplicationTests.java
@@ -1,5 +1,6 @@
 package via.doc1.dockertest;
 
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
@@ -8,6 +9,7 @@ class DockerTestApplicationTests {
 
 	@Test
 	void contextLoads() {
+		Assertions.assertTrue(true, "The context loads successfully.");
 	}
 
 }
